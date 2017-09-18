65fab2104b57db2ed80408cfc0a0ad83504087a4
albanie <albanie@robots.ox.ac.uk>
2017-09-18 09:52:02 +0100

    first commit

---
+++ setup_mcnColors.m
@@ -0,0 +1,10 @@
+function setup_mcnColors()
+%SETUP_MCNCOLORS Sets up mcnColors, by adding its folders 
+% to the Matlab path
+%
+% Copyright (C) 2017 Samuel Albanie
+% Licensed under The MIT License [see LICENSE.md for details]
+
+  root = fileparts(mfilename('fullpath')) ;
+  addpath(root, [root '/matlab']) ;
+  addpath( [root '/matlab/mex']) ;
