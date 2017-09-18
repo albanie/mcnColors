c75d9272ab9e8c5a93f4b2e617a1d6dbc0acdcc7
albanie <albanie@robots.ox.ac.uk>
2017-09-18 20:18:46 +0100

    simple demo

--- demo_rgb2hsv.m
+++ demo_rgb2hsv.m
@@ -1,10 +1,24 @@
 function demo_rgb2hsv(varargin)
 %DEMO_RGB2HSV demonstrate a color space transformation
+%   DEMO_RGB2HSV(VARARGIN) compute a color space transformations
+%   with `vl_rgb2hsv`.
+%
+%   DEMO_RGB2HSV(..., 'option', value, ...) accepts the following
+%   options:
+%
+%   `gpus`:: 1
+%    Device on which to run network 
+%
+% Copyright (C) 2017 Samuel Albanie
+% Licensed under The MIT License [see LICENSE.md for details]
 
   opts.gpus = 1 ;
   opts = vl_argparse(opts, varargin) ;
 
-  x = rand(500,500,3,1) ;
+  % First, to enable comparison with the native rgb2hsv function, we must 
+  % use a batch size of 1:
+  batchSize = 1 ;
+  x = rand(500,500,3,batchSize) ;
   if numel(opts.gpus), x = gpuArray(x) ; end
   tic ;
   y1 = rgb2hsv(x) ;
@@ -17,5 +31,14 @@ function demo_rgb2hsv(varargin)
   diff = abs(y2 - y1) ;
   fprintf('difference: %g\n', sum(diff(:))) ;
 
+  % print comparison on single image
   fprintf('native CPU rgb2hsv: %g\n', cpuTime) ;
   fprintf('GPU vl_rgb2hsv: %g\n', gpuTime) ;
+
+  % large batch sizes are also supported
+  batchSize = 5 ;
+  x = rand(500,500,3,batchSize) ;
+  tic 
+  y3 = vl_rgb2hsv(x) ; %#ok
+  gpuBatchTime = toc ;
+  fprintf('GPU vl_rgb2hsv: %g\n', gpuBatchTime) ;
