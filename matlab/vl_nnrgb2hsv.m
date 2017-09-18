b18066a1a42a210bc4a180975a0b5f302c1fce65
albanie <albanie@robots.ox.ac.uk>
2017-09-18 20:13:01 +0100

    add compile script and license

--- matlab/vl_nnrgb2hsv.m
+++ matlab/vl_nnrgb2hsv.m
@@ -1,8 +1,9 @@
 % VL_NNRGB2HSV Color space conversion.
-%   Y = VL_NNRGB2HSV(X) TODO(write docs)
-%  EXPECTS [R,G,B] values to lie in [0,1].  Returns 
-% [H,S,V] values in [0,360], [0,1], [0,1]
+%   Y = VL_NNRGB2HSV(X) converts a tensor of RGB images
+%   of shape H x W x 3 x N and converts them to the HSV color space.
+%   Each RGB value should lie in [0,1].  Following the convention used
+%   in the native `rgb2hsv` MATLAB function, elements in the output
+%   HSV space are also normalised to lie in [0,1].
 %
-% We take the HSV equivalent of zero tensor RGB image to also be a
-% zero tensor. Though, perhaps the hue should be undefined here (we 
-% are essentially taking 0/0 = 0)
+% Copyright (C) 2017 Samuel Albanie 
+% Licensed under The MIT License [see LICENSE.md for details]
